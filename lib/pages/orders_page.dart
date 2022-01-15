2eaa21d61a8c9b7933e10068d90ebbfa86705ae5
fabricio-silva-89 <fabriciojsilva89@gmail.com>
2022-01-15 09:31:38 -0300

    conclusão do módulo

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,56 +4,53 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_widget.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   const OrdersPage({Key? key}) : super(key: key);
 
-  @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Provider.of<OrderList>(context, listen: false).loadOrders().then((value) {
-      setState(() {
-        isLoading = false;
-      });
-    });
-  }
-
-  Future<void> _refreseOrders(BuildContext context) {
-    return Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders();
-  }
+  // Future<void> _refreseOrders(BuildContext context) {
+  //   return Provider.of<OrderList>(
+  //     context,
+  //     listen: false,
+  //   ).loadOrders();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : RefreshIndicator(
-              onRefresh: () => _refreseOrders(context),
-              child: ListView.builder(
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return Consumer<OrderList>(
+              builder: (ctx, orders, child) => ListView.builder(
                 itemCount: orders.itemsCount,
                 itemBuilder: (ctx, i) {
                   return OrderWidget(order: orders.items[i]);
                 },
               ),
-            ),
+            );
+          }
+        },
+      ),
+      // body: isLoading
+      //     ? const Center(
+      //         child: CircularProgressIndicator(),
+      //       )
+      //     : RefreshIndicator(
+      //         onRefresh: () => _refreseOrders(context),
+      //         child: ListView.builder(
+      //           itemCount: orders.itemsCount,
+      //           itemBuilder: (ctx, i) {
+      //             return OrderWidget(order: orders.items[i]);
+      //           },
+      //         ),
+      //       ),
     );
   }
 }
