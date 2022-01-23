d5e769a00f845ab60a06664b3eb0cab083f52727
fabricio-silva-89 <fabriciojsilva89@gmail.com>
2022-01-23 18:49:45 -0300

    Aula 23/01/2022

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/exceptions/auth_exception.dart';
 import 'package:shop/models/auth.dart';
 
 enum AuthMode { signup, login }
@@ -37,6 +36,21 @@ class _AuthFormState extends State<AuthForm> {
     });
   }
 
+  void _showErrorDialog(String msg) {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: const Text('Ocorreu um erro'),
+              content: Text(msg),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop,
+                  child: const Text('Fechar'),
+                )
+              ],
+            ));
+  }
+
   Future<void> submit() async {
     final isValid = formKey.currentState?.validate() ?? false;
 
@@ -47,16 +61,22 @@ class _AuthFormState extends State<AuthForm> {
     formKey.currentState?.save();
     Auth auth = Provider.of<Auth>(context, listen: false);
 
-    if (_isLogin()) {
-      await auth.login(
-        authData['email']!,
-        authData['password']!,
-      );
-    } else {
-      await auth.signUp(
-        authData['email']!,
-        authData['password']!,
-      );
+    try {
+      if (_isLogin()) {
+        await auth.login(
+          authData['email']!,
+          authData['password']!,
+        );
+      } else {
+        await auth.signUp(
+          authData['email']!,
+          authData['password']!,
+        );
+      }
+    } on AuthException catch (error) {
+      _showErrorDialog(error.toString());
+    } catch (error) {
+      _showErrorDialog('Ocorreu um erro desconhecido!');
     }
 
     setState(() => isLoading = false);
@@ -80,7 +100,7 @@ class _AuthFormState extends State<AuthForm> {
           child: Column(
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'E-mail'),
+                decoration: const InputDecoration(labelText: 'E-mail'),
                 keyboardType: TextInputType.emailAddress,
                 onSaved: (email) => authData['email'] = email ?? '',
                 validator: (emailValidator) {
